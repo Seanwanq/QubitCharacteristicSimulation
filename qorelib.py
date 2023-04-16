d03f0f11ddb204b0d1ae2b86aaa2e79bd4e79834
Sean Wang <seanwang313@outlook.com>
2023-04-16 20:29:36 +0800

    Finished 2d core library.

--- qorelib.py
+++ qorelib.py
@@ -1,14 +1,9 @@
 from scipy.linalg import expm
 import numpy as np
-from numpy import polyfit, poly1d
-from numpy import ndarray, matrix, complexfloating
-import plotly.graph_objects as go
-import random
-from tqdm import tqdm
-from scipy.optimize import leastsq
+from numpy import matrix
 
 
-def ComputeTDState(initialState: matrix, t: float, H: matrix):
+def ComputeState(initialState: matrix, t: float, H: matrix):
     tempMatrix: matrix = -1j * H * t
     timeState: matrix = expm(tempMatrix) @ initialState
     return timeState
@@ -16,4 +11,31 @@ def ComputeTDState(initialState: matrix, t: float, H: matrix):
 
 def ComputeStateProbability(state: matrix, chooser: int):
     if chooser == 0:
-        # probability: float =
+        probability: float = np.abs((state[0][0, 0]))**2
+        return probability
+    else:
+        probability: float = np.abs((state[1][0, 0]))**2
+        return probability
+
+
+def ComputeRamseyFinalStateWithoutNoise(initialState: matrix, H_d1: matrix, H_0: matrix, H_d2: matrix, A: float, τ: float, drive1Circle: float, drive2Circle: float):
+    T: float = (2 * np.pi) / (2 * 0.5 * np.sqrt(A**2))
+    t1: float = T * drive1Circle
+    t2: float = T * drive2Circle
+    firstState: matrix = ComputeState(initialState, t1, H_d1)
+    secondState: matrix = ComputeState(firstState, τ, H_0)
+    finalState: matrix = ComputeState(secondState, t2, H_d2)
+    return finalState
+
+
+def HamiltonianWithDrive2X2(omegaQ: float, omegaD: float, phiD: float, A: float):
+    delta: float = omegaD - omegaQ
+    H: matrix = np.matrix([[0.5 * delta, 0.5 * A * np.exp(1j * phiD)],
+                           [0.5 * A * np.exp(-1j * phiD), -0.5 * delta]])
+    return H
+
+
+def HamiltonianWithoutDrive2X2(omegaQ: float, omegaD: float):
+    delta: float = omegaD - omegaQ
+    H: matrix = np.matrix([[0.5 * delta, 0], [0, -0.5 * delta]])
+    return H
